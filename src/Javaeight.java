e6ab7ead990342fda480f979556479f073d0e3ee
Kolla.Loksainadh <loksainadh@gmail.com>
2025-06-02 16:27:28 +0530

    new  programs added

---
+++ src/Javaeight.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Javaeight {
+
+    public static void main(String[] args) {
+
+       // List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+       //     numbers.stream().filter(i->i%2!=0).collect(Collectors.toList()).forEach(i->System.out.println(i));
+
+ //int s = numbers.stream().mapToInt(Integer::intValue).sum();
+ //System.out.println(s);
+        //List<String> words = Arrays.asList("Apple", "Banana", "Avocado", "Peach", "Apricot", "Berry");
+             //       words.stream().filter(i->i.startsWith("A")).collect(Collectors.toList()).forEach(i->System.out.println(i));
+       // List<Integer> numbers = Arrays.asList(10, 20, 5, 40, 60, 30);
+      //  Optional<Integer> ss = numbers.stream().max(Integer::compareTo);
+     //   System.out.println(ss);
+
+//        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//        OptionalDouble ss = numbers.stream().filter(i -> i % 2 == 0).mapToInt(Integer::intValue).average();
+//                 System.out.println(ss);
+      //  List<String> words = Arrays.asList("java", "stream", "example");
+      //  words.stream().map(i->i.toUpperCase()).forEach(i->System.out.println(i));
+       // List<Integer> numbers = Arrays.asList(5, 3, 9, 5, 1, 3, 8);
+      //  numbers.stream().collect(Collectors.toSet()).stream().sorted(Comparator.reverseOrder()).forEach(i->System.out.println(i));
+       // List<String> words = Arrays.asList("apple", "bat", "batman", "cherry", "dog", "elephant");
+        // words.stream().collect(Collectors.groupingBy(String::length)).forEach((i,j)->System.out.println(i+""+j));
+        /*List<String> words = Arrays.asList("Hello", "World", "Java", "Stream");
+         String aa = words.stream().collect(Collectors.joining(" "));
+
+System.out.println(aa);*/
+
+     //  List<String> words = Arrays.asList("apple", "banana", "cherry", "date", "kiwi");
+     //   Optional<String> dd = words.stream().filter(i -> i.length() > 5).findFirst();
+       // System.out.println(dd);
+        //List<List<String>> listOfLists = Arrays.asList(Arrays.asList("apple", "banana"),    Arrays.asList("cherry", "date"),    Arrays.asList("kiwi", "lemon"));
+        //listOfLists.stream().flatMap(List::stream).collect(Collectors.toList()).forEach(i->System.out.println(i));
+      //  List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+       // numbers.stream().mapToDouble(Integer::doubleValue).forEach(i->System.out.println(i));
+
+    //    List<Integer> numbers = Arrays.asList(5, 7, 8, 9, 11, 12, 13);
+      //  Optional<Integer> dd = numbers.stream().filter(i -> i % 3 == 0).findFirst();
+       //    System.out.println(dd);
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
